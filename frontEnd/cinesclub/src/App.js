64d0ae6bc1fdd60fb92b8fbfc3d6831dc3b290db
Diego <dsimoes@correo.unimet.edu.ve>
2020-05-31 14:14:53 -0400

    Menu de la pagina

--- frontEnd/cinesclub/src/App.js
+++ frontEnd/cinesclub/src/App.js
@@ -1,10 +1,25 @@
 import React from 'react';
+import Header from './Componentes/Header/header.js';
 
 import './App.css';
 
 function App() {
   return (
     <div className="App">
+      <body className="cuerpo"> 
+
+      <div>
+        
+        <Header></Header>
+      </div>
+         
+        </body>
+       
+      
+
+
+      
+
      
     </div>
   );
---
+++ frontEnd/cinesclub/src/Componentes/Header/header.js
@@ -0,0 +1,36 @@
+import React, {Component} from 'react';
+import Barra from './menu.js';
+
+class Header extends Component{
+
+render()
+{
+
+    return(
+        <div>
+
+            <header className="header">
+
+            <div className="menu">
+                <Barra></Barra>
+            </div>
+
+
+
+            </header>
+
+
+        </div>
+
+
+
+    )
+}
+
+
+
+
+
+}
+
+export default Header;

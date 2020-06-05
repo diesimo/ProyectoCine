7263084eee86101dfc525df120241c2a5b3bea5b
Diego <dsimoes@correo.unimet.edu.ve>
2020-06-04 21:15:37 -0400

    Base de datos new

--- cinesclub/cine/admin.py
+++ cinesclub/cine/admin.py
@@ -1,15 +1,11 @@
 from django.contrib import admin
 from .models import Usuario
 from .models import Pelicula
-from .models import Producto
-from .models import Proveedor
-from .models import Factura
+
 
 admin.site.register(Usuario)
 admin.site.register(Pelicula)
-admin.site.register(Producto)
-admin.site.register(Proveedor)
-admin.site.register(Factura)
+
 
 
 # Register your models here.
--- cinesclub/cine/models.py
+++ cinesclub/cine/models.py
@@ -9,95 +9,37 @@ from django.db import models
 from multiselectfield import MultiSelectField
 from django.core.validators import MinValueValidator, MaxValueValidator
 
-class Usuario(models.Model):
-    cedula = models.IntegerField('Cedula',primary_key=True)
-    nombre = models.CharField('Nombre',max_length=20)
-    apellido = models.CharField('Apellido',max_length=20)
-    alias = models.CharField('User',max_length=20)
-    passw = models.CharField('Password',max_length=30)
-    email = models.CharField('Email',max_length=40)
-    telefono = models.IntegerField('Telefono')
-    fechain = models.DateField('Fecha de Nacimiento',db_column='fechaIn')  # Field name made lowercase.
-    fechana = models.DateField('Fecha de ingreso',db_column='fechaNa')  # Field name made lowercase.
-
-    class Meta:
-        managed = False
-        db_table = 'usuario'
-
-
-
-
 class Pelicula(models.Model):
-    categoria_choices=(
-        ('Terror', 'Terror'),
-        ('Comedia', 'Comedia'),
-        ('Romantico', 'Romantico'),
-        ('Suspenso', 'Suspenso'),
-        ('Drama','Drama'),
-        ('Ciencia Ficcion', 'Ciencia Ficcion'),
-        ('Accion', 'Accion'),
-        ('Aventura' ,' Aventura'),
-
-
-
-    )
-
-
-
-    idpelicula = models.AutoField('ID Pelicula',db_column='idPelicula', primary_key=True)  # Field name made lowercase.
-    clase = models.CharField('Tipo de Clase',max_length=4)
-    duracion = models.CharField('Duracion',max_length=10)
-    raking = models.IntegerField('Raking', validators=[MinValueValidator(1), MaxValueValidator(7)])
-    namedire = models.CharField('Nombre del Director',db_column='nameDire', max_length=20)  # Field name made lowercase.
-    secnamedire = models.CharField('Apellido del Director',db_column='secnameDire', max_length=20)  # Field name made lowercase.
+    idpelicula = models.AutoField('ID Pelicula',primary_key=True)
+    titulo = models.CharField('Titulo',max_length=40)
+    duracion = models.TimeField('Duracion')
     sinopsis = models.TextField('Sinopsis')
-    categoria = MultiSelectField('Categorias',choices=categoria_choices)
-    titulo = models.CharField('Titulo',max_length=50)
+    idioma = models.CharField('Idioma',max_length=15)
+    subtitulos = models.IntegerField('Subtitulos')
+    clase = models.CharField('Clase',max_length=2)
+    namedire = models.CharField('Nombre del Director',db_column='nameDire', max_length=15, blank=True, null=True)  # Field name made lowercase.     
+    apelldire = models.CharField('Apellido del Director',db_column='apellDire', max_length=15, blank=True, null=True)  # Field name made lowercase.   
+    raking = models.IntegerField('Raking')
+    categoria = models.CharField('Categoria',max_length=15)
 
     class Meta:
         managed = False
         db_table = 'pelicula'
 
 
-
-class Producto(models.Model):
-    namepro = models.CharField('Producto',db_column='namePro', max_length=20)  # Field name made lowercase.
-    costo = models.IntegerField('Costo')
-    tipopro = models.CharField('Tipo de Producto',db_column='tipoPro', max_length=30)  # Field name made lowercase.
-    stock = models.IntegerField('Stock')
-    idprov = models.ForeignKey('Proveedor', models.DO_NOTHING, db_column='idprov')
-
-    class Meta:
-        managed = False
-        db_table = 'producto'
-
-
-
-class Proveedor(models.Model):
-    idprov = models.AutoField('ID Provedor',primary_key=True)
-    nameprov = models.CharField('Nombre del Provedor',db_column='nameProv', max_length=40)  # Field name made lowercase.
-    direc = models.CharField('Direccion',db_column='Direc', max_length=200)  # Field name made lowercase.
-    rif = models.CharField('RIF',max_length=30)
+class Usuario(models.Model):
+    iduser = models.AutoField('ID User',primary_key=True)
+    fechaingreso = models.DateField('Fecha de Ingreso',db_column='fechaIngreso')  # Field name made lowercase.
+    passw = models.CharField('Password',max_length=20)
+    identi = models.CharField('User',max_length=20)
+    telefono = models.IntegerField('Telefono')
+    nombre = models.CharField('Nombre',max_length=20)
+    apellido = models.CharField('Apellido',max_length=20)
+    cedula = models.IntegerField('Cedula')
+    email = models.CharField('Email',max_length=30)
+    fechnaci = models.DateField('Fecha de Nacimiento',db_column='fechNaci')  # Field name made lowercase.
 
     class Meta:
         managed = False
-        db_table = 'proveedor'
-
-class Factura(models.Model):
-    idfac = models.AutoField('ID Factura',primary_key=True)
-    montobru = models.IntegerField('Monto Bruto',db_column='montoBru')  # Field name made lowercase.
-    montonet = models.IntegerField('Monto Neto',db_column='montoNet')  # Field name made lowercase.
-    descuento = models.IntegerField('Descuento')
-    datefac = models.DateField('Fecha de Factura',db_column='dateFac')  # Field name made lowercase.
-    ceduser = models.ForeignKey('Usuario', models.DO_NOTHING, db_column='cedUser')  # Field name made lowercase.
-    tippago = models.CharField('Tipo de Pago',db_column='tipPago', max_length=30)  # Field name made lowercase.
-    cantticket = models.IntegerField('Cantidad de boletos',db_column='cantTicket')  # Field name made lowercase.
-    funcion = models.CharField('Funcion',max_length=50, blank=True, null=True)
-    idfuncion = models.ForeignKey('Pelicula', models.DO_NOTHING, db_column='idfuncion', blank=True, null=True)
-    idprofac = models.ForeignKey('Producto', models.DO_NOTHING, db_column='idprofac', blank=True, null=True)
-
-    class Meta:
-        managed = False
-        db_table = 'factura'
-
+        db_table = 'usuario'
 
